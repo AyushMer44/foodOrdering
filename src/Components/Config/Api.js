af7f839a970d0ac459fb9b16a8e990b11acd0413
AyushMer44 <ayushmer5@gmail.com>
2024-10-03 00:14:09 +0530

    Change api url

--- src/Components/Config/Api.js
+++ src/Components/Config/Api.js
@@ -2,7 +2,7 @@
 
 import axios from "axios";
 
-export const API_URL="http://localhost:8080";
+export const API_URL="https://foodorderingbackend-production.up.railway.app/";
 // export const API_URL="http://localhost:8000/";
 
 export const api = axios.create({
